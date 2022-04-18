da7447bf720e447dc75c641b45cba66c985d8c3d
shearpaladin <sandy6@ualberta.ca>
2022-04-17 21:06:30 -0600

    finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-blog
+
+Created with Java, Maven, Thymeleaf, Spring Boot
--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,37 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
+
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap){
+        List<Post> allPosts = postRepository.getAllPosts();
+        modelMap.put("posts", allPosts);
+        return "home";
+    }
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
+        return "post-details";
+
+    }
+
+
 
 }
